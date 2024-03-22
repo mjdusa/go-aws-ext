57247c7a723e07ec5b029556986018a825cf5aa8
Michael Donahue <81647737+mdonahue-godaddy@users.noreply.github.com>
2024-03-22 18:59:38 -0500

    BASH Linter

--- install-deps.sh
+++ install-deps.sh
@@ -2,17 +2,17 @@
 
 GOBIN=${GOPATH}/bin
 
-if [ ! -f ${GOBIN}/golangci-lint ]; then
+if [ ! -f "${GOBIN}/golangci-lint" ]; then
 	echo "Installing ${GOBIN}/golangci-lint"
 	go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 fi
 
-if [ ! -f ${GOBIN}/gcov2lcov ]; then
+if [ ! -f "${GOBIN}/gcov2lcov" ]; then
 	echo "Installing ${GOBIN}/gcov2lcov"
 	go install github.com/jandelgado/gcov2lcov@latest
 fi
 
-if [ ! -f ${pwd}/.git/hooks/pre-commit ] && [ -f ${pwd}/hooks/pre-commit ]; then
+if [ ! -f "${pwd}/.git/hooks/pre-commit" ] && [ -f "${pwd}/hooks/pre-commit" ]; then
 	echo "Adding pre-commit hook to ${pwd}/.git/hooks/pre-commit"
-	ln -s ${pwd}/hooks/pre-commit ${pwd}/.git/hooks/pre-commit || true
+	ln -s "${pwd}/hooks/pre-commit" "${pwd}/.git/hooks/pre-commit" || true
 fi
